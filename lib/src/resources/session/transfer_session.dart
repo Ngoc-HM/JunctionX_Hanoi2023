298e79f6131122af80480a0205e5dddf728348a4
sclearner <71807587+sclearner@users.noreply.github.com>
2023-04-12 21:45:42 +0700

    Add transfer

--- lib/src/resources/session/transfer_session.dart
+++ lib/src/resources/session/transfer_session.dart
@@ -1,3 +1,4 @@
+import 'package:fintechdemo/src/blocs/inside_transfer_bloc.dart';
 import 'package:flutter/material.dart';
 
 class Transfer extends StatefulWidget {
@@ -7,8 +8,120 @@ class Transfer extends StatefulWidget {
 }
 
 class _Transfer extends State<Transfer> {
+  List<Widget> _feature = [
+    Inside(), ToBank(), ToCard()
+  ];
+  List<Widget> _featureName = [
+    Padding(
+      padding: EdgeInsets.symmetric(horizontal: 12),
+      child: Text('Nội bộ bank', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold), textAlign: TextAlign.center),
+    ),
+    Padding(
+      padding: EdgeInsets.symmetric(horizontal: 12),
+      child: Text('Chuyển tiền vào STK', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold), textAlign: TextAlign.center),
+    ),
+    Padding(
+      padding: EdgeInsets.symmetric(horizontal: 12),
+      child: Text('Chuyển tiền vào số thẻ', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold), textAlign: TextAlign.center),
+    ),
+  ];
+  List<bool> _isFeatureSelected = [true, false, false];
+  int _stateIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Hello, Transfer!"),);
+    return Wrap(
+      crossAxisAlignment: WrapCrossAlignment.center,
+      children: [
+        SizedBox(width: double.infinity, height: 10,),
+        Center(child: ToggleButtons(
+            constraints: BoxConstraints(maxWidth: 120, minHeight: 50, maxHeight: 50),
+            fillColor: Colors.lightGreen[700]!,
+            selectedColor: Colors.white,
+            selectedBorderColor: Colors.lightGreen[300]!,
+            onPressed: (int index) {
+              setState(() {
+                _stateIndex = index;
+                for (int i=0; i < _featureName.length; i++) _isFeatureSelected[i] = i == index;
+              });
+            },
+            children: _featureName,
+            isSelected: _isFeatureSelected)
+        ),
+        _feature[_stateIndex]
+      ],
+    );
+  }
+}
+
+class Inside extends StatefulWidget {
+  Inside({super.key});
+  @override
+  _Inside createState() => _Inside();
+}
+
+class _Inside extends State<Inside> {
+  String? _receiverName = "";
+  InsideReceiverBloc bloc = InsideReceiverBloc();
+  TextEditingController _receiverController = new TextEditingController();
+  var _receiverError = "Tài khoản không hợp lệ";
+  bool _receiverInvalid = false;
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      children: [
+        Padding(
+            padding: const EdgeInsets.fromLTRB(20, 0, 20, 10),
+            child: StreamBuilder(
+              stream: bloc.receiverStream,
+              builder: (context, snapshot) => TextField(
+                controller: _receiverController,
+                onEditingComplete: () {
+                  setState(() {
+                    String? receiver = bloc.isValidInfo(_receiverController.text);
+                    if (receiver != "" && receiver != Null) _receiverName = receiver;
+                    else showDialog(context: context,
+                        builder: (BuildContext context) => AlertDialog(
+                          title: Text("Lỗi"),
+                          content: const Text('Không có tài khoản đã nhập'),
+                          actions: <Widget>[
+                            TextButton(
+                              onPressed: () => Navigator.pop(context, 'Thoát'),
+                              child: const Text('Thoát'),
+                            ),
+                          ],
+                        ));
+                  });
+                },
+                style: TextStyle(fontSize: 18, color: Colors.black),
+                decoration: InputDecoration(
+                  labelText: "Tên tài khoản",
+                  errorText:
+                  snapshot.hasError ? snapshot.error.toString() : null,
+                  labelStyle:
+                  TextStyle(color: Color(0xff888888), fontSize: 15),
+                ),
+              ),
+            )),
+        Padding(
+          padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+          child: Text("$_receiverName", style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),),)
+
+      ],
+    );
+  }
+}
+
+class ToBank extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text("Tính năng đang phát triển"));
+  }
+}
+
+class ToCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text("Tính năng đang phát triển"));
   }
 }
--- lib/src/validators/validations.dart
+++ lib/src/validators/validations.dart
@@ -1,9 +1,18 @@
 class Validations {
+  static final Map<String, String> validReceiver = {
+    "0123456789": "VU THI HANG",
+    "0982934052": "HOANG NGOC MINH"
+  };
+
   static bool isValidUser(String user) {
     return user != null && user.length >= 6 && user.contains("@gmail.com");
   }
 
   static bool isValidPassword(String password) {
     return password != null && password.length >= 6;
+  }
+
+  static bool isValidReceiver(String receiver) {
+    return validReceiver.keys.contains(receiver);
   }
 }
