517422dc1d11ba3795a90947e52ced33c0677386
sclearner <71807587+sclearner@users.noreply.github.com>
2023-04-14 20:52:36 +0700

    Revert "Revert "Merge branch 'Sam_projectBranch'""
    This reverts commit 2db1509e14c97ae4a3cfc80e5f86a97d2578ef69.

--- lib/src/resources/session/home_session.dart
+++ lib/src/resources/session/home_session.dart
@@ -3,6 +3,10 @@ import 'package:intl/intl.dart';
 import 'package:fintechdemo/src/blocs/user_information.dart';
 import 'dart:math';
 
+class HomeDoNotDestroyOnLoad {
+  static bool canSee = false;
+}
+
 class Credit extends StatefulWidget {
   Credit({super.key});
   @override
@@ -11,14 +15,18 @@ class Credit extends StatefulWidget {
 
 class _Credit extends State<Credit> {
   User _user = testUser;
-  bool _canSee = false;
+  bool _canSee = HomeDoNotDestroyOnLoad.canSee;
 
   void _userWantToSee() {
     setState(() {
-      if (_canSee) _canSee = false;
+      if (_canSee) {
+        _canSee = false;
+        HomeDoNotDestroyOnLoad.canSee = false;
+      }
       else {
         //more security stuff
         _canSee = true;
+        HomeDoNotDestroyOnLoad.canSee = true;
       }
     });
   }
@@ -35,10 +43,12 @@ class _Credit extends State<Credit> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Wrap(
+      children: <Widget>[Container(
+      constraints: BoxConstraints(minHeight: 200, maxHeight: 200),
       height: 200,
       width: MediaQuery.of(context).size.width,
-      margin: EdgeInsets.fromLTRB(10, 10, 10, 0),
+      margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
       padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
       decoration: BoxDecoration(
           color: Colors.lightGreen.shade300,
@@ -61,8 +71,14 @@ class _Credit extends State<Credit> {
               _user.name,
               style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 35),
             ),
-          )
-          ,
+          ),
+        Expanded(
+          flex: 2,
+          child: Text(
+            "SĐT/Email đăng kí: ${_user.accountName}",
+            style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 17),
+          ),
+        ),
           Expanded(
               flex: 4,
               child: Row(
@@ -83,6 +99,7 @@ class _Credit extends State<Credit> {
           ,
         ],
       ),
-    );
+      )
+    ]);
   }
 }
