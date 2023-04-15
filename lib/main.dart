1a4bbd3a90bf19c63c59415f1781f67fbbb29809
NgocJQK <minhngocyn1@gmail.com>
2023-04-15 09:00:52 +0700

    check

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,11 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import './src/app.dart';
 import 'firebase_options.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(MyApp());
 }
+
 // bạn Hành đã ở đây !
--- lib/src/resources/login_page.dart
+++ lib/src/resources/login_page.dart
@@ -13,7 +13,8 @@ bool showPass = true;
 
 Route _createRoute() {
   return PageRouteBuilder(
-    pageBuilder: (context, animation, secondaryAnimation) => HomePage(pageIndex: 0),
+    pageBuilder: (context, animation, secondaryAnimation) =>
+        HomePage(pageIndex: 0),
     transitionsBuilder: (context, animation, secondaryAnimation, child) {
       const begin = Offset(0.0, 1.0);
       const end = Offset.zero;
@@ -55,11 +56,7 @@ class _LoginPage extends State<LoginPage> {
           children: <Widget>[
             // hình ảnh Flutter
             Padding(
-<<<<<<< HEAD
-              padding: const EdgeInsets.fromLTRB(0, 30, 0, 0),
-=======
               padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
->>>>>>> c37304ab69a279bb65c446b9879a8af61d72bfca
               child: Container(
                 //child: FlutterLogo(),
                 child: Image.asset(
